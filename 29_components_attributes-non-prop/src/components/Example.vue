ecd5731ff69036f0d0f398e0bf531983b388c4e6
Piotrek Zatorski <piotr.zat@gmail.com>
2020-04-12 21:54:23 +0200

    first commit

---
+++ 29_components_attributes-non-prop/src/components/Example.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>example</div>
+</template>
+
+<script>
+    export default {
+        name: "Example"
+    }
+</script>
+
+<style>
+    .kolor {
+        color: green !important;
+    }
+</style>
---
+++ 2_webpack_and_example_component/src/components/Example.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>{{message}}</p>
+</template>
+
+<script>
+    // export default - chcemy aby komponentent był  eskportowany domyślnie
+    export default {
+        data() {
+            return {
+                message: "Hello in example component"
+            }
+        }
+    }
+</script>
+<style>
+    p {
+        color: #41B883;
+    }
+</style>
